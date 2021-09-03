e7543e333b62303166065638e8ea2220f821ae1a
Hogjesty <64978483+Hogjesty@users.noreply.github.com>
2021-09-04 01:09:35 +0300

    Initial commit

---
+++ src/main/java/examples/Dish.java
@@ -0,0 +1,18 @@
+package examples;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class Dish {
+    private int id;
+    private String name;
+    private int cookingTime;
+    private double cost;
+    private int weight;
+
+}
---
+++ src/main/java/examples/FirsExpirience.java
@@ -0,0 +1,67 @@
+package examples;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class FirsExpirience {
+    private static final String DB_URL = "jdbc:mysql://localhost:3306/restaurant?serverTimezone=UTC";
+    private static final String USER = "root";
+    private static final String PASS = "root";
+    private static final String SELECT_QUERY = "SELECT * FROM dishes";
+    private static final String INSERT_QUERY =
+            "INSERT INTO `dishes` " +
+                    "(`name`, `cooking_time(mins)`, `cost`, `weight(grams)`) " +
+                    "VALUE (?, ?, ?, ?);";
+
+
+    public static void main(String[] args) {
+        Dish some = new Dish(0, "Samsa", 20, 50.5, 300);
+        createDish(some);
+//        List<Dish> dishes = getDishes();
+//        dishes.forEach(System.out::println);
+        System.out.println(some);
+        Dish dish = new Dish();
+
+    }
+
+
+    public static List<Dish> getDishes() {
+        List<Dish> list = new ArrayList<>();
+        try (Connection conn = DriverManager.getConnection(DB_URL, USER, PASS);
+             Statement stmt = conn.createStatement();
+             ResultSet rs = stmt.executeQuery(SELECT_QUERY)) {
+            while (rs.next()) {
+                list.add(new Dish(
+                        rs.getInt("id"),
+                        rs.getString("name"),
+                        rs.getInt(3),
+                        rs.getDouble("cost"),
+                        rs.getInt(5)
+                ));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return list;
+    }
+
+    public static void createDish(Dish dish) {
+
+        try (Connection con = DriverManager.getConnection(DB_URL, USER, PASS);
+             PreparedStatement prstm = con.prepareStatement(INSERT_QUERY, Statement.RETURN_GENERATED_KEYS)) {
+            prstm.setString(1, dish.getName());
+            prstm.setInt(2, dish.getCookingTime());
+            prstm.setDouble(3, dish.getCost());
+            prstm.setInt(4, dish.getWeight());
+            prstm.executeUpdate();
+            ResultSet resultSet = prstm.getGeneratedKeys();
+            if (resultSet.next()) {
+                dish.setId(resultSet.getInt(1));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
