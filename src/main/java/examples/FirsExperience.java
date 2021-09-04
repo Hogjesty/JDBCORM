1aff7b52707bec3835bf6273b7583991c52577b0
Hogjesty <64978483+Hogjesty@users.noreply.github.com>
2021-09-04 20:18:27 +0300

    impl delete and update

--- src/main/java/examples/FirsExperience.java
+++ src/main/java/examples/FirsExperience.java
@@ -4,24 +4,33 @@ import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
-public class FirsExpirience {
+public class FirsExperience {
     private static final String DB_URL = "jdbc:mysql://localhost:3306/restaurant?serverTimezone=UTC";
     private static final String USER = "root";
     private static final String PASS = "root";
     private static final String SELECT_QUERY = "SELECT * FROM dishes";
-    private static final String INSERT_QUERY =
-            "INSERT INTO `dishes` " +
-                    "(`name`, `cooking_time(mins)`, `cost`, `weight(grams)`) " +
-                    "VALUE (?, ?, ?, ?);";
+    private static final String INSERT_QUERY = "INSERT INTO `dishes` " +
+            "(`name`, `cooking_time(mins)`, `cost`, `weight(grams)`) " +
+            "VALUE (?, ?, ?, ?);";
+    private static final String DELETE_QUERY = "DELETE FROM `restaurant`.`dishes`" +
+            "WHERE `id` = ?;";
+    private static final String UPDATE_QUERY = "UPDATE `restaurant`.`dishes` " +
+            "SET `name` = ?, `cooking_time(mins)` = ?, `cost` = ?, `weight(grams)` = ? " +
+            "WHERE `id` = ?;";
 
 
     public static void main(String[] args) {
-        Dish some = new Dish(0, "Samsa", 20, 50.5, 300);
-        createDish(some);
+//        Dish some = new Dish(0, "Samsa", 20, 50.5, 300);
+//        createDish(some);
+
 //        List<Dish> dishes = getDishes();
 //        dishes.forEach(System.out::println);
-        System.out.println(some);
-        Dish dish = new Dish();
+//        System.out.println(some);
+
+//        deleteDish(7);
+
+//        Dish samsa = new Dish(0, "Samsa", 20, 51.0, 300);
+//        updateDish(6, samsa);
 
     }
 
@@ -63,5 +72,29 @@ public class FirsExpirience {
             e.printStackTrace();
         }
 
+    }
+
+    public static void deleteDish(int id) {
+        try (Connection con = DriverManager.getConnection(DB_URL, USER, PASS);
+             PreparedStatement statement = con.prepareStatement(DELETE_QUERY)) {
+            statement.setInt(1, id);
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void updateDish(int id, Dish dish) {
+        try (Connection con = DriverManager.getConnection(DB_URL, USER, PASS);
+             PreparedStatement statement = con.prepareStatement(UPDATE_QUERY)) {
+            statement.setString(1, dish.getName());
+            statement.setInt(2, dish.getCookingTime());
+            statement.setDouble(3, dish.getCost());
+            statement.setInt(4, dish.getWeight());
+            statement.setInt(5, id);
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
